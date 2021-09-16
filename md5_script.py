e8fec9561798f1e11d269192c4348f8fd63c8d0f
Levi Fackler <levi.fackler@gmail.com>
2021-09-16 17:52:53 -0400

    work around
    work around

--- md5_script.py
+++ md5_script.py
@@ -2,9 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 import hashlib
 
+
+url = 'http://46.101.14.69:30636' # YOUR IP HERE
 # Grab html
 
-raw_html = requests.get('http://[IP_GOES_HERE]').text
+raw_html = requests.get(url).text
 
 #Grab text from specific element in html (.text) and return string
 soup = BeautifulSoup(raw_html, 'lxml')
@@ -14,6 +16,10 @@ print(str_from_html)
 #Use hashlib to encrypt string with md5
 
 result = hashlib.md5(str_from_html.encode())
-print(result.hexdigest())
+hashed = result.hexdigest()
 
 # Post the returned hexdigest
+
+r = requests.post(url, data=hashed)
+
+print(r.content)
